b1bfe470a9d6dd17ca267b837a3179ad98a96341
drHyperion451 <doctorhyperion451@gmail.com>
2023-11-04 19:04:32 +0000

    Fixed some bugs and implementing game parsing

--- dosbox.py
+++ dosbox.py
@@ -3,6 +3,8 @@ import subprocess
 from pathlib import Path
 from configparser import ConfigParser
 
+from shlex import split as shlex_split
+
 def open_or_generate(config_path):
     if config_path.is_file() == False:
         config = ConfigParser()
@@ -14,6 +16,24 @@ def open_or_generate(config_path):
             config.write(config_file)
     pass
 
+def game(sourcePort:str, iwad:str, extraPwads:str | None = None) -> str:
+
+    cmdString = f"{sourcePort} -iwad {iwad}"
+    if extraPwads != None:
+        cmdString += f" -file {extraPwads}"
+
+    cmdString += f' {ARGS}'
+    # Splits into arguments. Just because subprocess.run needs this.
+    cmdList = shlex_split(cmdString)
+
+
+    completed_process = subprocess.run(cmdList, capture_output=True, text=True)
+    
+    # This will handle properly if errors had happened or not.
+    O_STDERR = completed_process.stderr
+    O_STDOUT = completed_process.stdout
+    print(O_STDERR, O_STDOUT)
+
 if __name__ == '__main__':
     config_path = Path('./wrapper.ini')
     config = ConfigParser()
@@ -23,8 +43,8 @@ if __name__ == '__main__':
     EXEC = config['DEFAULT']['exec']
     ARGS = config['DEFAULT']['args']
     skip_next = False
-
-    new_args = [EXEC, ARGS]
+    debug = False
+    new_args = []
 
     for arg in sys.argv[1:]:
         if skip_next:
@@ -33,7 +53,11 @@ if __name__ == '__main__':
         if arg in ["-conf", "-exit", "--fullscreen"]:
             skip_next = True
             continue
+        if arg in ["--debugwrapper"]:
+            skip_next = True
+            debug = True
+            continue
         new_args.append(arg)
 
-
-        subprocess.run(new_args)
+        game(EXEC, ARGS)
+        if debug: input()
